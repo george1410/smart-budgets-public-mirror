4bb45f8b0507e6d5289c91032b65af10c19808c7
Raigo Jerva <raigo.jerva@gmail.com>
2019-02-10 23:41:17 +0000

    update global style

--- src/style.js
+++ src/style.js
@@ -1,22 +1,29 @@
 import { createGlobalStyle } from 'styled-components';
+import media from './util/mediaQueries';
 
 export const GlobalStyle = createGlobalStyle`
   html {
     font-size: 62.5%;
-    font-family: ${props => props.theme.font};
-
-    /* @media only screen and (max-width: ${props => props.theme.bpMedium}) {
+    ${media.tablet`
       font-size: 56.25%;
-    } */
+    `}
+  }
+
+  body {
+    font-family: ${props => props.theme.font};
   }
 `;
 
 export const theme = {
-  primaryColor: 'rgb(12, 160, 255)',
-  greyLight: '#dbdbdb',
-  grey: '#484848',
+  primaryBlue: '#0056FF',
+  primaryBlueDark: '#052B80',
+  error: '#E41317',
+  greyLightest: '#ddd',
+  greyLight: '#888',
+  grey: '#555',
   greyDark: '#222',
   black: '#000',
+  offWhite: '#eee',
   white: '#fff',
   font: 'Montserrat',
 };
