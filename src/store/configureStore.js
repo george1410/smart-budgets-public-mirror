2d56a25d0a1c86b7c6ed67c04994c2599b35a029
Raigo Jerva <raigo.jerva@gmail.com>
2019-02-17 15:23:54 +0000

    remove files left over from demo

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -5,8 +5,6 @@ import {
   compose,
 } from 'redux';
 import reduxThunk from 'redux-thunk';
-import counterReducer from '../reducers/counterReducer';
-import answerReducer from '../reducers/answerReducer';
 import authReducer from '../reducers/authReducer';
 
 /* eslint-disable no-underscore-dangle */
@@ -15,8 +13,6 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => createStore(
   combineReducers({
-    counter: counterReducer,
-    answer: answerReducer,
     auth: authReducer,
   }),
   composeEnhancers(applyMiddleware(reduxThunk)),
