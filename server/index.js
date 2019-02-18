a070409b1085f353420e3c2c1bac8fdb6d3832e2
George McCarron <hello@georgemccarron.com>
2019-02-18 21:00:37 +0000

    added some basic validation

--- server/index.js
+++ server/index.js
@@ -78,6 +78,7 @@ app.get('/api/users/:id/transactions', (req, res) => {
  */
 app.get('/api/users/:id/categories', (req, res) => {
   let sql;
+  let badRequest = false;
   if (req.query.period) {
     let { period } = req.query;
     period = period.toUpperCase();
@@ -102,6 +103,9 @@ app.get('/api/users/:id/categories', (req, res) => {
           transactions.date <= CURDATE()
         GROUP BY categories.displayName
       `;
+    } else {
+      badRequest = true;
+      res.status(400).json({ error: 'Bad Request. Invalid period.' });
     }
   } else {
     sql = `
@@ -115,56 +119,57 @@ app.get('/api/users/:id/categories', (req, res) => {
   `;
   }
 
+  if (!badRequest) {
+    let res1 = [];
+    conn.query(sql, (err, results) => {
+      if (err) throw err;
 
-  let res1 = [];
-  conn.query(sql, (err, results) => {
-    if (err) throw err;
-
-    res1 = results;
-    res1.forEach((result, key) => {
-      res1[key].id = result.id.split(',').map(Number);
+      res1 = results;
+      res1.forEach((result, key) => {
+        res1[key].id = result.id.split(',').map(Number);
+      });
+      res1 = results;
     });
-    res1 = results;
-  });
 
-  sql = `
-    SELECT SUM(budgets.budget) AS budget, categories.displayName, GROUP_CONCAT(DISTINCT categories.categoryId) AS id FROM budgets 
-    JOIN categories ON categories.categoryId = budgets.categoryId
-    WHERE budgets.userId = ${req.params.id}
-    GROUP BY categories.displayName
-  `;
+    sql = `
+      SELECT SUM(budgets.budget) AS budget, categories.displayName, GROUP_CONCAT(DISTINCT categories.categoryId) AS id FROM budgets 
+      JOIN categories ON categories.categoryId = budgets.categoryId
+      WHERE budgets.userId = ${req.params.id}
+      GROUP BY categories.displayName
+    `;
 
-  conn.query(sql, (err, results) => {
-    if (err) throw err;
-    if (results.length < 1) {
-      res.status(404).json({ error: 'No results were found.' });
-    } else {
-      const out = [];
-      results.forEach((result) => {
-        let found = false;
-        res1.forEach((group) => {
-          if (result.displayName === group.displayName) {
-            found = true;
+    conn.query(sql, (err, results) => {
+      if (err) throw err;
+      if (results.length < 1) {
+        res.status(404).json({ error: 'No results were found.' });
+      } else {
+        const out = [];
+        results.forEach((result) => {
+          let found = false;
+          res1.forEach((group) => {
+            if (result.displayName === group.displayName) {
+              found = true;
+              out.push({
+                budget: result.budget,
+                spend: group.spend,
+                displayName: result.displayName,
+                id: result.id.split(',').map(Number),
+              });
+            }
+          });
+          if (!found) {
             out.push({
               budget: result.budget,
-              spend: group.spend,
+              spend: 0,
               displayName: result.displayName,
               id: result.id.split(',').map(Number),
             });
           }
         });
-        if (!found) {
-          out.push({
-            budget: result.budget,
-            spend: 0,
-            displayName: result.displayName,
-            id: result.id.split(',').map(Number),
-          });
-        }
-      });
-      res.json(out);
-    }
-  });
+        res.json(out);
+      }
+    });
+  }
 });
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
