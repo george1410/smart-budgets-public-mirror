615bf8ababcc8cdc3abf559ec0546cc248909c5d
Raigo Jerva <raigo.jerva@gmail.com>
2019-03-03 14:52:26 +0000

    wrap in try-catch, as token might not be in header

--- server/middleware/idMatcher.js
+++ server/middleware/idMatcher.js
@@ -3,12 +3,15 @@ const config = require('../config');
 
 const idMatcher = (req, res, next) => {
   const token = req.headers.authorization;
-  const idOnToken = jwt.verify(token, config.secret).sub;
-  if (idOnToken === parseInt(req.params.id, 10)) {
-    next();
-  } else {
-    // next();
-    res.send({ error: 'token does not match id on route' });
+  try {
+    const idOnToken = jwt.verify(token, config.secret).sub;
+    if (idOnToken === parseInt(req.params.id, 10)) {
+      next();
+    } else {
+      res.send({ error: 'token does not match id on route' });
+    }
+  } catch (err) {
+    res.send('unauthorized');
   }
 };
 
