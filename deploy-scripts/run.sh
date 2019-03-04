bc4e09510861fc7c46994e0d65ede0ed043a72fc
George McCarron <hello@georgemccarron.com>
2019-03-04 11:16:43 +0000

    fixing env bug when deploying

--- deploy-scripts/run.sh
+++ deploy-scripts/run.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 cd ~/smart-budgets
 export PORT=80
+export DB_HOST=smartbudgets.co6kasjhhncd.eu-west-1.rds.amazonaws.com
+export DB_USER=smartbudgets
+export DB_PASSWORD=capitalone
+export DB_DATABASE=smartbudgets
+export DB_PORT=8080
 pm2 start server
