7a816be705dcc8f4676475d713b0c33ae05ff792
psyns6 <psyns6@exmail.nottingham.ac.uk>
2019-03-11 17:13:35 +0000

    sql budget statemnets

--- server/apiRouter.js
+++ server/apiRouter.js
@@ -181,4 +181,67 @@ module.exports = (app) => {
       });
     }
   });
+
+   //a simple query dividing the sum by 4 between the two dates provided
+   app.get('/api/users/:id/budget', (req, res) => {
+    let querySelect;
+        querySelect = `
+          SELECT ROUND(SUM(amount) / 4)
+          FROM transactions
+          WHERE userId = ${req.params.id} 
+          AND date BETWEEN DATE_SUB(CURDATE(), INTERVAL 4 MONTH)
+          AND CURDATE()
+          `;
+  
+          conn.query(querySelect, (error, results) => {
+            if (error) throw error;
+            if (results.length < 1) {
+              res.status(404).json({ error: 'No results were found.' });
+            } else {
+              res.json(results);
+            }
+          });
+        });
+   
+  // user id and cateorgory will be sent in
+  app.get('/api/users/:id/:category/budget', (req, res) => {
+    let querySelect;
+        querySelect = `
+          SELECT *
+          FROM transactions
+          WHERE userId = ${req.params.id} 
+          AND categoryId = ${req.params.category}
+          `;
+  
+          conn.query(querySelect, (error, results) => {
+            if (error) throw error;
+            if (results.length < 1) {
+              res.status(404).json({ error: 'No results were found.' });
+            } else {
+              res.json(results);
+            }
+          });
+        });
+
+  // budget for category
+  app.get('/api/users/:id/:category/budget', (req, res) => {
+    let querySelect;
+        querySelect = `
+        SELECT ROUND(SUM(amount) / 4)
+        FROM transactions
+        WHERE userId = ${req.params.id} 
+        AND categoryId = ${req.params.category}
+        AND date BETWEEN DATE_SUB(CURDATE(), INTERVAL 4 MONTH)
+        AND CURDATE()
+        `;
+  
+          conn.query(querySelect, (error, results) => {
+            if (error) throw error;
+            if (results.length < 1) {
+              res.status(404).json({ error: 'No results were found.' });
+            } else {
+              res.json(results);
+            }
+          });
+        });
 };
