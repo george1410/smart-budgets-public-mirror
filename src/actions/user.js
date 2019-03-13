7ab10fc225484533e7e2905198d4e58d3b51b799
Raigo Jerva <raigo.jerva@gmail.com>
2019-03-13 22:51:30 +0000

    update category data, when period is changed

--- src/actions/user.js
+++ src/actions/user.js
@@ -2,6 +2,7 @@ import {
   SET_USER_INFO, USER_INFO_ERROR, USER_INFO_LOADING, SWITCH_PERIOD,
 } from './types';
 import api from '../api/api';
+import { startSetCategories } from './categories';
 
 export const setUserInfo = userInfo => ({
   type: SET_USER_INFO,
@@ -17,16 +18,25 @@ export const switchUserPeriods = () => ({
   type: SWITCH_PERIOD,
 });
 
-export const updatePeriod = () => (_, getState) => {
-  const { period } = getState().user;
-  const { uid } = getState().auth;
-  api.post(`api/users/${uid}`, { period });
-};
-
 export const setUserInfoLoading = status => ({
   type: USER_INFO_LOADING,
   status,
 });
+
+export const updatePeriod = () => (dispatch, getState) => {
+  dispatch(setUserInfoLoading(true));
+  const { period } = getState().user;
+  const { uid } = getState().auth;
+  api.post(`api/users/${uid}`, { period })
+    .then(() => {
+      dispatch(startSetCategories());
+    })
+    .catch(() => {
+      dispatch(setUserInfoLoading(false));
+      dispatch(setUserInfoError(true));
+    });
+};
+
 
 export const startSetUserInfo = () => (dispatch, getState) => {
   dispatch(setUserInfoLoading(true));
