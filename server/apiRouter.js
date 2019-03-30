e18e72a410c0735bae17b05ef400b4de0f98fb08
George McCarron <hello@georgemccarron.com>
2019-03-30 23:44:47 +0000

    removing budget calculating code from web api

--- server/apiRouter.js
+++ server/apiRouter.js
@@ -263,44 +263,6 @@ module.exports = (app) => {
     }
   });
 
-  // a simple query dividing the sum by 4 between the two dates provided
-  app.get('/api/users/:id/budget', (req, res) => {
-    const querySelect = `
-          SELECT ROUND(SUM(amount) / 4)
-          FROM transactions
-          WHERE userId = ${req.params.id} 
-          AND date BETWEEN DATE_SUB(CURDATE(), INTERVAL 4 MONTH) AND CURDATE()
-          `;
-
-    pool.query(querySelect, (error, results) => {
-      if (error) throw error;
-      if (results.length < 1) {
-        res.status(404).json({ error: 'No results were found.' });
-      } else {
-        res.json(results);
-      }
-    });
-  });
-
-  // budget for category
-  app.get('/api/users/:id/:category/budget', (req, res) => {
-    const querySelect = `
-        SELECT ROUND(SUM(amount) / 4)
-        FROM transactions
-        WHERE userId = ${req.params.id} 
-        AND categoryId = ${req.params.category}
-        AND date BETWEEN DATE_SUB(CURDATE(), INTERVAL 4 MONTH) AND CURDATE()
-        `;
-
-    pool.query(querySelect, (error, results) => {
-      if (error) throw error;
-      if (results.length < 1) {
-        res.status(404).json({ error: 'No results were found.' });
-      } else {
-        res.json(results);
-      }
-    });
-  });
   /**
    * POST route for adding a friend.
    * Endpoint: /api/users/{userid}/friends
--- server/budgets.js
+++ server/budgets.js
@@ -9,3 +9,44 @@ exports.setBudgets = (userId) => {
         GROUP BY displayName
     `;
 };
+
+/*
+  // a simple query dividing the sum by 4 between the two dates provided
+  app.get('/api/users/:id/budget', (req, res) => {
+    const querySelect = `
+          SELECT ROUND(SUM(amount) / 4)
+          FROM transactions
+          WHERE userId = ${req.params.id} 
+          AND date BETWEEN DATE_SUB(CURDATE(), INTERVAL 4 MONTH) AND CURDATE()
+          `;
+
+    pool.query(querySelect, (error, results) => {
+      if (error) throw error;
+      if (results.length < 1) {
+        res.status(404).json({ error: 'No results were found.' });
+      } else {
+        res.json(results);
+      }
+    });
+  });
+
+  // budget for category
+  app.get('/api/users/:id/:category/budget', (req, res) => {
+    const querySelect = `
+        SELECT ROUND(SUM(amount) / 4)
+        FROM transactions
+        WHERE userId = ${req.params.id} 
+        AND categoryId = ${req.params.category}
+        AND date BETWEEN DATE_SUB(CURDATE(), INTERVAL 4 MONTH) AND CURDATE()
+        `;
+
+    pool.query(querySelect, (error, results) => {
+      if (error) throw error;
+      if (results.length < 1) {
+        res.status(404).json({ error: 'No results were found.' });
+      } else {
+        res.json(results);
+      }
+    });
+  });
+  */
