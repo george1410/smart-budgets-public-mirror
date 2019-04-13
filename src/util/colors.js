0ca54c505ca5fa491e43bdda3317c44ad4fc2ff9
Raigo Jerva <raigo.jerva@gmail.com>
2019-04-13 23:41:17 +0300

    adjust colors

--- src/util/colors.js
+++ src/util/colors.js
@@ -1,38 +1,38 @@
 const colors = {
   BILLS: {
-    color: 'rgba(69, 0, 255, 0.9)',
+    color: 'rgba(0, 119, 255, 0.9)',
     text: 'white',
   },
   CASH: {
-    color: 'rgba(101, 0, 255, 0.9)',
+    color: 'rgba(0, 72, 255, 0.9)',
     text: 'white',
   },
   'EATING OUT': {
-    color: 'rgba(139, 0, 255, 0.9)',
+    color: 'rgba(0, 60, 200, 0.9)',
     text: 'white',
   },
   GROCERIES: {
-    color: 'rgba(283, 0, 255, 0.9)',
+    color: 'rgba(69, 0, 255, 0.9)',
     text: 'white',
   },
   HEALTH: {
-    color: 'rgba(233, 0, 255, 0.9)',
+    color: 'rgba(101, 0, 255, 0.9)',
     text: 'white',
   },
   LEISURE: {
-    color: 'rgba(255, 0, 223, 0.9)',
+    color: 'rgba(139, 0, 255, 0.9)',
     text: 'white',
   },
   MISCELLANEOUS: {
-    color: 'rgba(255, 0, 167, 0.9)',
-    text: 'white',
-  },
-  SHOPPING: {
     color: 'rgba(255, 0, 110, 0.9)',
     text: 'white',
   },
-  TRAVEL: {
+  SHOPPING: {
     color: 'rgba(255, 34, 56, 0.9)',
+    text: 'white',
+  },
+  TRAVEL: {
+    color: 'rgba(255, 0, 0, 0.9)',
     text: 'white',
   },
 };
